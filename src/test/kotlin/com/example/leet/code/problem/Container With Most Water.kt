f95bc813af81a4945f1051189f059a65aa24292d
Maxim Tsoy <mtsoy1990@gmail.com>
2021-01-14 14:37:08 +0300

    optimize Longest Palindromic Substring

--- src/test/kotlin/com/example/leet/code/problem/Container With Most Water.kt
+++ src/test/kotlin/com/example/leet/code/problem/Container With Most Water.kt
@@ -11,27 +11,34 @@ class `Container With Most Water` {
 
     @Test
     fun test1() {
-        Assertions.assertEquals(maxArea(intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)), 49)
+        Assertions.assertEquals(49, maxArea(intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)))
     }
 
     @Test
     fun test2() {
-        Assertions.assertEquals(maxArea(intArrayOf(1, 1)), 1)
+        Assertions.assertEquals(1, maxArea(intArrayOf(1, 1)))
     }
 
     @Test
     fun test3() {
-        Assertions.assertEquals(maxArea(intArrayOf(4, 3, 2, 1, 4)), 16)
+        Assertions.assertEquals(16, maxArea(intArrayOf(4, 3, 2, 1, 4)))
     }
 
     fun maxArea(height: IntArray): Int {
-        var result = 0;
+        var result = 0
+        var start = 0
+        var end = height.size - 1
 
-        for (i in height.indices) {
-            for (j in i + 1 until height.size) {
-                val tmp = min(height[i], height[j]) * (j - i)
-                println("$i + $j = $tmp (" + min(height[i], height[j]) + ") (" + (j - i) + ")")
-                result = max(tmp, result)
+        while (start < end) {
+            val l = height[start]
+            val r = height[end]
+            val tmp = min(l, r) * (end - start)
+            result = max(tmp, result)
+
+            if (l > r) {
+                end--
+            } else {
+                start++
             }
         }
 
