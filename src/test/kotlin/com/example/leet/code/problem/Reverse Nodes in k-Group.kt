45a5b25d0691523b0ecdac5becde4a8d8d0c21ec
Maxim <mtsoy1990@gmail.com>
2021-01-24 15:24:37 +0300

    added Reverse Nodes in k-Group

---
+++ src/test/kotlin/com/example/leet/code/problem/Reverse Nodes in k-Group.kt
@@ -0,0 +1,90 @@
+package com.example.leet.code.problem
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+class `Reverse Nodes in k-Group` {
+
+    @Test
+    fun test1() {
+        val root = ListNode(1)
+        val l2 = ListNode(2)
+        val l3 = ListNode(3)
+        val l4 = ListNode(4)
+        val l5 = ListNode(5)
+        root.next = l2
+        l2.next = l3
+        l3.next = l4
+        l4.next = l5
+
+        val result = reverseKGroup(root, 3)
+        Assertions.assertEquals(3, result!!.`val`)
+        Assertions.assertEquals(2, result.next!!.`val`)
+        Assertions.assertEquals(1, result.next!!.next!!.`val`)
+        Assertions.assertEquals(4, result.next!!.next!!.next!!.`val`)
+        Assertions.assertEquals(5, result.next!!.next!!.next!!.next!!.`val`)
+        Assertions.assertNull(result.next!!.next!!.next!!.next!!.next)
+    }
+
+    @Test
+    fun test2() {
+        val root = ListNode(1)
+        val l2 = ListNode(2)
+        val l3 = ListNode(3)
+        val l4 = ListNode(4)
+        val l5 = ListNode(5)
+        root.next = l2
+        l2.next = l3
+        l3.next = l4
+        l4.next = l5
+
+        val result = reverseKGroup(root, 2)
+        Assertions.assertEquals(2, result!!.`val`)
+        Assertions.assertEquals(1, result.next!!.`val`)
+        Assertions.assertEquals(4, result.next!!.next!!.`val`)
+        Assertions.assertEquals(3, result.next!!.next!!.next!!.`val`)
+        Assertions.assertEquals(5, result.next!!.next!!.next!!.next!!.`val`)
+        Assertions.assertNull(result.next!!.next!!.next!!.next!!.next)
+    }
+
+    data class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    private fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+        if (head?.next == null) {
+            return head
+        }
+
+        val root = ListNode(-1)
+        root.next = head
+        var current: ListNode? = root
+
+
+        while (current != null) {
+            val tmpList = mutableListOf<ListNode>()
+            var tmpNode: ListNode? = current.next
+            for (i in 0 until k) {
+                if (tmpNode == null) {
+                    return root.next
+                }
+                tmpList.add(tmpNode)
+                tmpNode = tmpNode.next
+            }
+
+            val firstNode = tmpList.first()
+            val lastNode = tmpList.last()
+
+            for (i in k-1 downTo 1) {
+                tmpList[i].next = tmpList[i - 1]
+            }
+
+            tmpList[0].next = tmpNode
+            current.next = lastNode
+            current = firstNode
+        }
+
+        return root.next
+    }
+}
