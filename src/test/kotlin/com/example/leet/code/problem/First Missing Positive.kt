15b37e0de7b800a1bf69b604e2d8706bf926bdfd
Maxim Tsoy <mtsoy1990@gmail.com>
2021-02-05 13:32:32 +0300

    added hard optimisation

--- src/test/kotlin/com/example/leet/code/problem/First Missing Positive.kt
+++ src/test/kotlin/com/example/leet/code/problem/First Missing Positive.kt
@@ -31,6 +31,31 @@ class `First Missing Positive` {
         Assertions.assertEquals(4, firstMissingPositive(intArrayOf(0, 1, 1, 1, 1, 2, 3)))
     }
 
+    @Test
+    fun test12() {
+        Assertions.assertEquals(4, firstMissingPositive2(intArrayOf(1, 2, 3)))
+    }
+
+    @Test
+    fun test22() {
+        Assertions.assertEquals(3, firstMissingPositive2(intArrayOf(2, 4, 1)))
+    }
+
+    @Test
+    fun test32() {
+        Assertions.assertEquals(1, firstMissingPositive2(intArrayOf(7, 8, 9)))
+    }
+
+    @Test
+    fun test42() {
+        Assertions.assertEquals(4, firstMissingPositive2(intArrayOf(0, 1, 2, 3)))
+    }
+
+    @Test
+    fun test52() {
+        Assertions.assertEquals(4, firstMissingPositive2(intArrayOf(0, 1, 1, 1, 1, 2, 3)))
+    }
+
     private fun firstMissingPositive(nums: IntArray): Int {
         var result = 1
         val all = nums
@@ -41,5 +66,24 @@ class `First Missing Positive` {
             .all { it == result++ }
 
         return if (all) result else result - 1
+    }
+
+    //hard optimisation
+    private fun firstMissingPositive2(nums: IntArray): Int {
+        val bitSet = BooleanArray(301)
+
+        nums.forEach {
+            if (it in 1..300) {
+                bitSet[it - 1] = true
+            }
+        }
+
+        for (i in bitSet.indices) {
+            if (!bitSet[i]) {
+                return i + 1
+            }
+        }
+
+        return 301
     }
 }
