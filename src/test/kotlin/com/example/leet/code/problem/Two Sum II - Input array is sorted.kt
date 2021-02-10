d98d208cf5ba7e1e3b511fd6366c9bdd0aabf083
Maxim Tsoy <mtsoy1990@gmail.com>
2021-02-10 18:51:51 +0300

    optimisation Two Sum II - Input array is sorted

--- src/test/kotlin/com/example/leet/code/problem/Two Sum II - Input array is sorted.kt
+++ src/test/kotlin/com/example/leet/code/problem/Two Sum II - Input array is sorted.kt
@@ -2,6 +2,7 @@ package com.example.leet.code.problem
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.*
 
 @Suppress("ClassName")
 class `Two Sum II - Input array is sorted` {
@@ -11,16 +12,18 @@ class `Two Sum II - Input array is sorted` {
         Assertions.assertArrayEquals(intArrayOf(1, 2), twoSum(intArrayOf(2, 7, 11, 15), 9))
     }
 
+    @Test
+    fun test2() {
+        Assertions.assertArrayEquals(intArrayOf(1, 3), twoSum(intArrayOf(2, 3, 4), 6))
+    }
+
 
     private fun twoSum(numbers: IntArray, target: Int): IntArray {
 
-        for (i in numbers.indices) {
-            val a = numbers[i]
-            for (j in i + 1..numbers.lastIndex) {
-                val b = numbers[j]
-                if (a + b == target) {
-                    return intArrayOf(i + 1, j + 1)
-                }
+        for (i in 0 until numbers.lastIndex) {
+            val secondIndex = Arrays.binarySearch(numbers, i + 1, numbers.size, target - numbers[i])
+            if (secondIndex > 0) {
+                return intArrayOf(i + 1, secondIndex + 1)
             }
         }
         return intArrayOf(0, 0)
