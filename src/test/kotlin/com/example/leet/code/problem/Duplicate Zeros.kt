e0934a0c223102f64c767b2abb08d911f62e81b2
Maxim Tsoy <mtsoy1990@gmail.com>
2021-03-01 07:49:43 +0300

    refactored Duplicate Zeros

--- src/test/kotlin/com/example/leet/code/problem/Duplicate Zeros.kt
+++ src/test/kotlin/com/example/leet/code/problem/Duplicate Zeros.kt
@@ -27,32 +27,59 @@ class `Duplicate Zeros` {
     fun test3() {
         val arr = intArrayOf(8, 4, 5, 0, 0, 0, 0, 7)
         duplicateZeros(arr)
+        Assertions.assertArrayEquals(intArrayOf(8, 4, 5, 0, 0, 0, 0, 0), arr)
+    }
 
+    @Test
+    fun test4() {
+        val arr = intArrayOf(8, 4, 5, 0, 0, 1, 2, 3, 4, 5)
+        duplicateZeros(arr)
+        Assertions.assertArrayEquals(intArrayOf(8, 4, 5, 0, 0, 0, 0, 1, 2, 3), arr)
+    }
+
+    @Test
+    fun test5() {
+        val arr = intArrayOf(1, 0, 2, 0, 1)
+        duplicateZeros(arr)
+        Assertions.assertArrayEquals(intArrayOf(1, 0, 0, 2, 0), arr)
+    }
+
+    @Test
+    fun test6() {
+        val arr = intArrayOf(0, 1, 7, 6, 0, 2, 0, 7)
+        duplicateZeros(arr)
+        println(arr.contentToString())
+        Assertions.assertArrayEquals(intArrayOf(0, 0, 1, 7, 6, 0, 0, 2), arr)
+    }
+
+    @Test
+    fun test7() {
+        val arr = intArrayOf(8, 4, 5, 0, 0, 0, 0, 7)
+        duplicateZeros(arr)
+        println(arr.contentToString())
         Assertions.assertArrayEquals(intArrayOf(8, 4, 5, 0, 0, 0, 0, 0), arr)
     }
 
 
     private fun duplicateZeros(arr: IntArray): Unit {
-        var count = arr.count { it == 0 }
+        val count = arr.count { it == 0 }
         val size = arr.size
+        var end = size - 1
+        var newEnd = size + count - 1
 
-        for (i in arr.lastIndex downTo 0) {
-            if (count == 0) {
-                return
+        while (end != newEnd) {
+
+            if (newEnd < size) {
+                arr[newEnd] = arr[end]
             }
-
-            if (arr[i] == 0) {
-                if (i + count < size) {
-                    arr[i + count] = arr[i]
+            newEnd--
+            if (arr[end] == 0) {
+                if (newEnd < size) {
+                    arr[newEnd] = arr[end]
                 }
-                count--
-                if (i + count < size) {
-                    arr[i + count] = arr[i]
-                }
-            } else if (i + count < size) {
-                arr[i + count] = arr[i]
+                newEnd--
             }
-
+            end--
         }
     }
 }
