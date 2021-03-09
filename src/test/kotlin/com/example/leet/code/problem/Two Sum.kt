527a0745c2cd94bee82f599e6fcef36842b6c1cb
Maxim Tsoy <mtsoy1990@gmail.com>
2021-03-09 08:08:03 +0300

    added Two Sum

---
+++ src/test/kotlin/com/example/leet/code/problem/Two Sum.kt
@@ -0,0 +1,33 @@
+package com.example.leet.code.problem
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+class `Two Sum` {
+
+    @Test
+    fun test1() {
+        Assertions.assertArrayEquals(intArrayOf(0, 1), twoSum(intArrayOf(2, 7, 11, 15), 9))
+    }
+
+    @Test
+    fun test2() {
+        Assertions.assertArrayEquals(intArrayOf(1, 2), twoSum(intArrayOf(3, 2, 4), 6))
+    }
+
+
+    private fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            val x = target - nums[i]
+            if (map.contains(x)) {
+                return intArrayOf(map[x]!!, i)
+            }
+
+            map[nums[i]] = i
+        }
+
+        return intArrayOf()
+    }
+}
