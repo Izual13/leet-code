79c64f3d0791653ebef96b1a0ac555a3cf307aa7
Maxim Tsoy <mtsoy1990@gmail.com>
2021-03-15 10:15:13 +0300

    refactored Longest Substring Without Repeating Characters

--- src/test/kotlin/com/example/leet/code/problem/Longest Substring Without Repeating Characters.kt
+++ src/test/kotlin/com/example/leet/code/problem/Longest Substring Without Repeating Characters.kt
@@ -2,31 +2,39 @@ package com.example.leet.code.problem
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import kotlin.math.max
 
 @Suppress("ClassName")
 class `Longest Substring Without Repeating Characters` {
 
     @Test
     fun test1() {
-        Assertions.assertEquals(lengthOfLongestSubstring("aab"), 2)
+        Assertions.assertEquals(2, lengthOfLongestSubstring("aab"))
+    }
+
+    @Test
+    fun test2() {
+        Assertions.assertEquals(3, lengthOfLongestSubstring("pwwkew"))
+    }
+
+    @Test
+    fun test3() {
+        Assertions.assertEquals(3, lengthOfLongestSubstring("dvdf"))
     }
 
     private fun lengthOfLongestSubstring(s: String): Int {
-        val set = LinkedHashSet<Int>()
-        var max = 0
-        s.chars().forEach {
-            if (!set.add(it)) {
-                val iterator = set.iterator()
+        val map = HashMap<Char, Int>()
+        var result = 0
 
-                while (iterator.next() != it) {
-                    iterator.remove()
-                }
-                iterator.remove()
-                set.add(it)
+        var start = 0
+        for (i in s.indices) {
+            if (map.containsKey(s[i])) {
+                start = max(start, map[s[i]]!!)
             }
-            max = kotlin.math.max(set.size, max)
+            result = max(result, i - start + 1)
+            map[s[i]] = i + 1
         }
 
-        return max
+        return result
     }
 }
