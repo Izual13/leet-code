9f1472598c194243fee8ca4f4e8cff25c60baca3
Maxim <mtsoy1990@gmail.com>
2021-04-18 15:56:20 +0300

    added Check if the Sentence Is Pangram

---
+++ src/test/kotlin/com/example/leet/code/problem/Check if the Sentence Is Pangram.kt
@@ -0,0 +1,29 @@
+package com.example.leet.code.problem
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+@Suppress("ClassName")
+class `Check if the Sentence Is Pangram` {
+
+    @Test
+    fun test1() {
+        Assertions.assertTrue(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
+    }
+
+    @Test
+    fun test2() {
+        Assertions.assertFalse(checkIfPangram("a"))
+    }
+
+
+    private fun checkIfPangram(sentence: String): Boolean {
+        val array = BooleanArray(26)
+        for (i in sentence) {
+            array[i.toInt() - 'a'.toInt()] = true
+        }
+
+        return array.count { it } == 26
+    }
+}
