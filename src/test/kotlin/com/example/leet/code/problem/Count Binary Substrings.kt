02a549f38f6b478e72f35ae2ea12c93ba83b2784
Maxim <mtsoy1990@gmail.com>
2021-04-25 22:39:24 +0300

    added Count Binary Substrings

---
+++ src/test/kotlin/com/example/leet/code/problem/Count Binary Substrings.kt
@@ -0,0 +1,35 @@
+package com.example.leet.code.problem
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+class `Count Binary Substrings` {
+
+    @Test
+    fun test1() {
+        Assertions.assertEquals(6, countBinarySubstrings("00110011"))
+    }
+
+    @Test
+    fun test2() {
+        Assertions.assertEquals(4, countBinarySubstrings("10101"))
+    }
+
+
+    private fun countBinarySubstrings(s: String): Int {
+        var result = 0
+        for (i in s.indices) {
+            val main = s[i]
+            for (j in i downTo 0) {
+                if (main == s[j] && i + i - j + 1 <= s.lastIndex && s[i + i - j + 1] != main) {
+                    result++
+                } else {
+                    break
+                }
+            }
+        }
+
+        return result
+    }
+}
