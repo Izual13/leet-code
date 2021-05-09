595edff777afbf571ea3907eb75bde035121de76
Maxim <mtsoy1990@gmail.com>
2021-05-09 22:18:36 +0300

    added Shuffle the Array

---
+++ src/test/kotlin/com/example/leet/code/problem/Shuffle the Array.kt
@@ -0,0 +1,25 @@
+package com.example.leet.code.problem
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+@Suppress("ClassName")
+class `Shuffle the Array` {
+
+    @Test
+    fun test1() {
+        Assertions.assertArrayEquals(intArrayOf(2, 3, 5, 4, 1, 7), shuffle(intArrayOf(2, 5, 1, 3, 4, 7), 3))
+    }
+
+
+    private fun shuffle(nums: IntArray, n: Int): IntArray {
+        val result = IntArray(nums.size)
+        var counter = 0
+        for (i in 0 until n) {
+            result[counter++] = nums[i]
+            result[counter++] = nums[i + n]
+        }
+        return result
+    }
+}
