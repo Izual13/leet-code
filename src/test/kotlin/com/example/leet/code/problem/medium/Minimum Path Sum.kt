ea8ac3d80311789a1364d5a3f4c7333ddd076ea1
Maxim <mtsoy1990@gmail.com>
2022-06-10 02:05:22 +0300

    optimisation

--- src/test/kotlin/com/example/leet/code/problem/medium/Minimum Path Sum.kt
+++ src/test/kotlin/com/example/leet/code/problem/medium/Minimum Path Sum.kt
@@ -2,6 +2,7 @@ package com.example.leet.code.problem.medium
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.LinkedList
 import kotlin.math.min
 
 @Suppress("ClassName")
@@ -14,28 +15,62 @@ class `Minimum Path Sum` {
 
     @Test
     fun test2() {
-        Assertions.assertEquals(12, minPathSum(arrayOf(intArrayOf(1, 2, 3), intArrayOf(4,5,6))))
+        Assertions.assertEquals(12, minPathSum(arrayOf(intArrayOf(1, 2, 3), intArrayOf(4, 5, 6))))
     }
 
+    @Test
+    fun test3() {
+        Assertions.assertEquals(0, minPathSum(arrayOf(intArrayOf(0))))
+    }
 
-    private fun minPathSum(grid: Array<IntArray>, x: Int = 0, y: Int = 0, result: Int = 0): Int {
-        val newResult = result + grid[y][x]
-        if (x == grid[0].lastIndex && y == grid.lastIndex) {
-            return newResult
+    @Test
+    fun test4() {
+        Assertions.assertEquals(6, minPathSum(arrayOf(intArrayOf(1, 2, 3))))
+    }
+
+    private fun minPathSum(grid: Array<IntArray>): Int {
+
+        val q: LinkedList<Pair<Int, Int>> = LinkedList()
+
+        val cache: Array<IntArray> = Array(grid.size) { IntArray(grid[0].size) }
+
+        if (grid.lastIndex > 0) {
+            q.push(0 to 1)
         }
 
-        if (x == grid[0].lastIndex) {
-            return minPathSum(grid, x, y + 1, newResult)
+        if (grid[0].lastIndex > 0) {
+            q.push(1 to 0)
         }
 
-        if (y == grid.lastIndex) {
-            return minPathSum(grid, x + 1, y, newResult)
+        cache[0][0] = grid[0][0]
+
+        while (q.isNotEmpty()) {
+            val (x, y) = q.removeLast()
+
+            if (cache[y][x] != 0) {
+                continue
+            }
+
+            val c = grid[y][x]
+            if (x == 0) {
+                cache[y][x] = cache[y - 1][x] + c
+            } else if (y == 0) {
+                cache[y][x] = cache[y][x - 1] + c
+            } else {
+                cache[y][x] = min(cache[y - 1][x] + c, cache[y][x - 1] + c)
+            }
+
+            if (y == grid.lastIndex && x == grid[0].lastIndex) {
+                return cache[grid.lastIndex][grid[0].lastIndex]
+            } else if (y == grid.lastIndex) {
+                q.push(x + 1 to 0)
+            } else if (x == grid[0].lastIndex) {
+                q.push(x to y + 1)
+            } else {
+                q.push(x + 1 to y)
+                q.push(x to y + 1)
+            }
         }
-
-
-        val r1 = minPathSum(grid, x + 1, y, newResult)
-        val r2 = minPathSum(grid, x, y + 1, newResult)
-
-        return min(r1, r2)
+        return cache[grid.lastIndex][grid[0].lastIndex]
     }
 }
