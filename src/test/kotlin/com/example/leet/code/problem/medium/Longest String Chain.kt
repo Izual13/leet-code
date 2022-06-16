40e57d70c2cbaf798de140d3fbc3c0038c26bb08
Maxim <mtsoy1990@gmail.com>
2022-06-16 23:39:27 +0300

    added Longest String Chain with cache

--- src/test/kotlin/com/example/leet/code/problem/medium/Longest String Chain.kt
+++ src/test/kotlin/com/example/leet/code/problem/medium/Longest String Chain.kt
@@ -14,37 +14,51 @@ class `Longest String Chain` {
 
     @Test
     fun test2() {
-        Assertions.assertEquals(5, longestStrChain(arrayOf("xbc","pcxbcf","xb","cxbc","pcxbc")))
+        Assertions.assertEquals(5, longestStrChain(arrayOf("xbc", "pcxbcf", "xb", "cxbc", "pcxbc")))
+    }
+
+    @Test
+    fun test3() {
+        Assertions.assertEquals(1, longestStrChain(arrayOf("abcd", "dbqca")))
+    }
+
+    @Test
+    fun test4() {
+        Assertions.assertEquals(2, longestStrChain(arrayOf("a", "b", "ab", "bac")))
+    }
+
+    @Test
+    fun test5() {
+        Assertions.assertEquals(4, longestStrChain(arrayOf("a","ab","ac","bd","abc","abd","abdd")))
     }
 
 
     private fun longestStrChain(words: Array<String>): Int {
         val map = words.asSequence().groupBy { it.length }.toSortedMap()
-        var result = 1
+        val cache = HashMap<String, String>()
+        var result = 0
         for (word in words) {
-            result = max(result, longestWord(word, map).length - word.length)
+            result = max(result, longestWord(word, map, cache).length - word.length)
         }
 
-        return result+1
+        return result + 1
     }
 
-    private fun longestWord(current: String, map: Map<Int, List<String>>): String {
+    private fun longestWord(current: String, map: Map<Int, List<String>>, cache: HashMap<String, String>): String {
         val list = map[current.length + 1] ?: return current
+
+        val cached = cache[current]
+        if (cached != null) {
+            return cached
+        }
+
         var result = current
         for (word in list) {
             var errorCount = 0
             var startShort = 0
             var startLong = 0
             while (startLong <= word.lastIndex) {
-                if (errorCount > 1) {
-                    break
-                }
-
-                if (startLong == word.lastIndex) {
-                    val tmp = longestWord(word, map)
-                    if (tmp.length > result.length) {
-                        result = tmp
-                    }
+                if (errorCount > 1 || (errorCount==0 && startLong == word.lastIndex)) {
                     break
                 }
 
@@ -55,8 +69,19 @@ class `Longest String Chain` {
                     startShort++
                     startLong++
                 }
+
+            }
+
+            if (errorCount <= 1) {
+                val tmp = longestWord(word, map, cache)
+                if (tmp.length > result.length) {
+                    result = tmp
+                }
+                break
             }
         }
+
+        cache[current] = result
 
         return result
     }
