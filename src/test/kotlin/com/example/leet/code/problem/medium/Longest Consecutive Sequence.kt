901b2c045dcd187b79833154bf5b7b5428f3772a
Maxim <mtsoy1990@gmail.com>
2022-06-20 21:54:22 +0300

    hotfix Longest Consecutive Sequence

--- src/test/kotlin/com/example/leet/code/problem/medium/Longest Consecutive Sequence.kt
+++ src/test/kotlin/com/example/leet/code/problem/medium/Longest Consecutive Sequence.kt
@@ -2,6 +2,7 @@ package com.example.leet.code.problem.medium
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 import kotlin.math.max
 
 
@@ -18,6 +19,14 @@ class `Longest Consecutive Sequence` {
         Assertions.assertEquals(9, longestConsecutive(intArrayOf(0, 3, 7, 2, 5, 8, 4, 6, 0, 1)))
     }
 
+    @Test
+    @Timeout(2)
+    fun test3() {
+        val result = IntArray(100000) { it }
+
+        Assertions.assertEquals(100000, longestConsecutive(result))
+    }
+
 
     private fun longestConsecutive(nums: IntArray): Int {
         val set = HashSet<Int>()
@@ -28,6 +37,10 @@ class `Longest Consecutive Sequence` {
         var result = 0
 
         for (i in set) {
+            if (set.contains(i - 1)) {
+                continue
+            }
+
             var tmp = 1
             var value = i + 1
             while (set.contains(value)) {
