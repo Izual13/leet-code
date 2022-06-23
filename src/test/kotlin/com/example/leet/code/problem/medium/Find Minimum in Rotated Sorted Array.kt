d3f97c40f326da5aea557112a3a5ecbd91a48839
Maxim <mtsoy1990@gmail.com>
2022-06-24 02:49:22 +0300

    hotfix Find Minimum in Rotated Sorted Array

--- src/test/kotlin/com/example/leet/code/problem/medium/Find Minimum in Rotated Sorted Array.kt
+++ src/test/kotlin/com/example/leet/code/problem/medium/Find Minimum in Rotated Sorted Array.kt
@@ -48,11 +48,11 @@ class `Find Minimum in Rotated Sorted Array` {
             val mid = start + (end - start) / 2
 
             if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
+                return nums[mid + 1]
             }
 
             if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
+                return nums[mid]
             }
 
             if (nums[mid] > nums[0]) {
