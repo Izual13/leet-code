676e7ee97cfc74fd00b7389f2c0f6b7545c43ac7
Maxim <mtsoy1990@gmail.com>
2022-06-25 01:51:51 +0300

    fixed Partition Equal Subset Sum

--- src/test/kotlin/com/example/leet/code/problem/medium/Partition Equal Subset Sum.kt
+++ src/test/kotlin/com/example/leet/code/problem/medium/Partition Equal Subset Sum.kt
@@ -2,6 +2,7 @@ package com.example.leet.code.problem.medium
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 
 @Suppress("ClassName")
 class `Partition Equal Subset Sum` {
@@ -17,13 +18,16 @@ class `Partition Equal Subset Sum` {
     }
 
     @Test
+    @Timeout(2)
     fun test3() {
-        val ints = intArrayOf(
-            100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-            100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-            100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-            100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-            100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97)
+        val ints = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+        Assertions.assertEquals(true, canPartition(ints))
+    }
+
+    @Test
+    @Timeout(2)
+    fun test4() {
+        val ints = intArrayOf(1,2,5)
         Assertions.assertEquals(false, canPartition(ints))
     }
 
@@ -35,34 +39,23 @@ class `Partition Equal Subset Sum` {
         }
 
         val half = sum / 2
-        nums.sort()
-
-        return find(nums, half)
-    }
-
-    private fun find(nums: IntArray, sum: Int): Boolean {
-        if (sum == 0) {
-            return true
-        } else if (sum < 0) {
-            return false
-        }
 
         var set = HashSet<Int>()
         set.add(0)
 
         for (n in nums) {
+            val newSet = HashSet<Int>()
             for (j in set) {
                 val tmp = j + n
-                if (sum == tmp) {
+                if (half == tmp) {
                     return true
                 }
-                val newSet = HashSet<Int>()
                 newSet.add(tmp)
                 newSet.add(j)
-                set = newSet
             }
+            set = newSet
         }
 
-        return false
+        return set.contains(half)
     }
 }
