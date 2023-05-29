070de2c63ce4ebdc14e244d4171cc774393b2b21
Maxim Tsoy <mtsoy1990@gmail.com>
2023-05-29 10:45:59 +0600

    added day12

--- src/test/kotlin/com/example/leet/code/days/Day12.java
+++ src/test/kotlin/com/example/leet/code/days/Day12.java
@@ -17,4 +17,28 @@ public class Day12 {
         }
         return result;
     }
+
+    //n
+    public int[] minOperations2(String boxes) {
+        int size = boxes.length();
+        int[] r = new int[size];
+        int c=0;
+        int o=0;
+
+        for(int i=0;i<size;i++){
+            r[i]+=o;
+            c+=boxes.charAt(i)-'0';
+            o+=c;
+        }
+
+        c=0;
+        o=0;
+        for(int i=size-1;i>=0;i--){
+            r[i]+=o;
+            c+=boxes.charAt(i)-'0';
+            o+=c;
+        }
+
+        return r;
+    }
 }
